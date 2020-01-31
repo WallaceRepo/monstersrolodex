a4492798a5caa25af3da010b9519f82387d911b8
WallaceRepo <sophiawallacedev@gmail.com>
2020-01-31 17:50:15 -0600

    Updates

---
+++ precache-manifest.effc7d08e09211c354ac951551bdc882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa6f81db343eef057cda123e04432b5",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "5eb4fb6d986ad2d7a4cc",
+    "url": "/monstersrolodex/static/css/main.9f75c251.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monstersrolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstersrolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb4fb6d986ad2d7a4cc",
+    "url": "/monstersrolodex/static/js/main.2a04cf68.chunk.js"
+  },
+  {
+    "revision": "7866796e6fc4260a18cc",
+    "url": "/monstersrolodex/static/js/runtime-main.dbd6355d.js"
+  }
+]);
